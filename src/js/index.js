2f75080e8b09f034a592d1fd7ba6adc4bad30493
Carl Avidano <cavidano@gmail.com>
2020-03-25 05:57:12 -0400

    adding modal scripts

--- src/js/index.js
+++ src/js/index.js
@@ -171,13 +171,69 @@ window.onload = () => {
     // E. External Links
     //////////////////////////////////////////////
 
-    window.onload = () => {
+    const externalLinks = document.querySelectorAll('[data-link="external"]');
 
-        const externalLinks = document.querySelectorAll('[data-link="external"]');
+    for (const link of externalLinks) {
+        link.insertAdjacentHTML('beforeend', '<span class="sr-only">Opens a new window</span>');
+    }
 
-        for (const link of externalLinks) {
-            link.insertAdjacentHTML('beforeend', '<span class="sr-only">Opens a new window</span>');
+    //////////////////////////////////////////////
+    // F. Modal
+    //////////////////////////////////////////////
+
+    var focusedElementBeforeModal, modal, registerButton;
+
+    var KEY = {
+        ESCAPE: 27,
+        TAB: 9
+    }
+
+    // Required null checks
+
+    if (document.querySelector('.modal') && document.querySelector('.open-modal')) {
+        modal = document.querySelector('.modal');
+        registerButton = document.querySelector('.open-modal');
+        registerButton.addEventListener('click', openModal);
+    }
+
+    function openModal() {
+
+        // element that was focused before modal opened
+        // we will focus back on this when modal is closed
+        focusedElementBeforeModal = document.activeElement;
+
+        modal.style.display = 'block';
+        // add the key listener  
+        modal.addEventListener('keydown', processEscapeTabKeys);
+
+        // Find all focusable modal elements
+        var modalElements = 'input:not([disabled]), button:not([disabled])';
+        var focusableElements = modal.querySelectorAll(modalElements);
+
+        // Convert NodeList to Array only for IE 11
+        //focusableElements = Array.prototype.slice.call(focusableElements);
+
+        var firstElementOfModal = focusableElements[0];
+        var lastElementOfModal = focusableElements[focusableElements.length - 1];
+
+        // Focus on first element of the modal - firstName
+        firstElementOfModal.focus();
+
+        function processEscapeTabKeys(event) {
+            // handle TAB key
+            if (event.keyCode === KEY.TAB) {
+                if (document.activeElement === lastElementOfModal) {
+                    event.preventDefault();
+                    firstElementOfModal.focus();
+                }
+            }
+            // handle ESCAPE key to close the modal
+            if (event.keyCode === KEY.ESCAPE) {
+                modal.style.display = 'none';
+                // Focus to the register button (the last focus before modal was opened)
+                focusedElementBeforeModal.focus();
+            }
         }
-    };
+    }
 
 }; /* window.load */
