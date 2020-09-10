a56bfe22eb701505c5d5f40a44affed365c3a3b6
Carl Avidano <cavidano@gmail.com>
2020-09-10 07:09:33 -0400

    added spinner js!

--- src/js/index.js
+++ src/js/index.js
@@ -596,6 +596,108 @@ import 'element-closest-polyfill';
         
     });
 
+    // Input Number Spinner
+
+    const numberInputList = document.querySelectorAll(".form__entry--number");
+
+    numberInputList.forEach((numberInput) => {
+
+        const spinnerInput = numberInput.querySelector("input[type='number']");
+        const btnAdd = numberInput.querySelector("[data-type='add']");
+        const btnSubtract = numberInput.querySelector("[data-type='subtract']");
+
+        var minLimit, maxLimit, step;
+
+        function init() {
+
+            minLimit = makeNumber(getAttribute(spinnerInput, 'min')) || 0,
+                maxLimit = makeNumber(getAttribute(spinnerInput, 'max')) || 9000,
+                step = makeNumber(getAttribute(spinnerInput, 'step') || '1');
+
+            btnSubtract.addEventListener('click', changeSpinner, false);
+            btnAdd.addEventListener('click', changeSpinner, false);
+            btnSubtract.addEventListener('keyup', keySpinner, false);
+            btnAdd.addEventListener('keyup', keySpinner, false);
+
+            if (supportsTouch()) {
+                btnSubtract.addEventListener('touchend', removeClickDelay, false);
+                btnAdd.addEventListener('touchend', removeClickDelay, false);
+            }
+
+            if (supportsPointer()) {
+                btnSubtract.addEventListener('pointerup', removeClickDelay, false);
+                btnAdd.addEventListener('pointerup', removeClickDelay, false);
+            }
+        }
+
+        function removeClickDelay(e) {
+            e.preventDefault();
+            e.target.click();
+        }
+
+        function makeNumber(inputString) {
+            return parseInt(inputString, 10);
+        }
+
+        function update(direction) {
+            var num = makeNumber(spinnerInput.value);
+            if (direction === 'add') {
+                console.log("neat");
+                spinnerInput.value = ((num + step) <= maxLimit) ? (num + step) : spinnerInput.value;
+            } else if (direction === 'subtract') {
+                spinnerInput.value = ((num - step) >= minLimit) ? (num - step) : spinnerInput.value;
+            }
+        }
+
+        function getAttribute(el, attr) {
+            var hasGetAttr = (el.getAttribute && el.getAttribute(attr)) || null;
+            if (!hasGetAttr) {
+                var attrs = el.attributes;
+                for (var i = 0, len = attrs.length; i < len; i++) {
+                    if (attrs[i].nodeName === attr) {
+                        hasGetAttr = attrs[i].nodeValue;
+                    }
+                }
+            }
+            return hasGetAttr;
+        }
+
+        /* Touch and Pointer support */
+        function supportsTouch() {
+            return ('ontouchstart' in window);
+        }
+
+        function supportsPointer() {
+            return ('pointerdown' in window);
+        }
+
+        /* Keyboard support */
+        function keySpinner(e) {
+            switch (e.keyCode) {
+                case 40:
+                case 37: // Down, Left
+                    update('subtract');
+                    btnSubtract.focus();
+                    break;
+                case 38:
+                case 39:
+                    // Top, Right
+                    update('add');
+                    btnAdd.focus();
+                    break;
+            }
+        }
+
+        function changeSpinner(e) {
+            e.preventDefault();
+            var increment = getAttribute(e.target, 'data-type');
+            update(increment);
+        }
+
+        init();
+
+    });
+
     //////////////////////////////////////////////
     // External Links
     //////////////////////////////////////////////
