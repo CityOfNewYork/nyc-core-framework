7242bdb735157b671eda607ac6fd273d783bcca5
Carl Avidano <cavidano@gmail.com>
2020-12-24 11:31:49 -0500

    fixed icons

--- src/js/icon_display.js
+++ src/js/icon_display.js
@@ -23,7 +23,6 @@ let currentView = "Grid";
 
 const displayIcons = (icons) => {
 
-    
     const iconGridHTML = icons.map((iconArray) => {
 
         let name = iconArray.properties.name;
@@ -31,43 +30,62 @@ const displayIcons = (icons) => {
 
         return `
         <div class="grid__cell">
-            <div class="aspect-ratio aspect-ratio--1by1 border margin-bottom-1">
+
+            <div class="aspect-ratio aspect-ratio--1by1 theme-light margin-bottom-1">
                 <div class="aspect-ratio__content display-flex justify-content-center align-items-center">
                     <span class="nyc_icon_${name}" style="font-size: 2em !important;"></span>   
                 </div>
             </div>
+        
             <p class="font-size-sm">
-                    ${name}
-                </p>
+                ${name}
+            </p>
+
         </div>`;
 
     }).join('');
 
-    const iconListHTML = icons.map((iconArray) => {
+    const iconListHTML = icons.map((iconArray, index) => {
 
         let name = iconArray.properties.name;
         let tags = iconArray.icon.tags;
 
-        return `
-        <div class="grid__cell display-flex theme-dark rounded">
-            
-            <div class="width-25">
-                <div class="aspect-ratio aspect-ratio--3by2 border-right box-shadow-2">
-                    <div class="aspect-ratio__content display-flex justify-content-center align-items-center">
-                        <span class="nyc_icon_${name}" style="font-size: 2em !important;"></span>   
+        let htmlSpan = `
+            <span class="token tag">
+                <span class="token tag"><span class="token punctuation">&lt;</span>span </span>
+                <span class="token attr-name">class</span>
+                <span class="token attr-value">
+                    <span class="token punctuation attr-equals">=</span>
+                    <span class="token punctuation">"</span>
+                    nyc_icon_${name}
+                    <span class="token punctuation">"</span>
+                </span>
+                <span class="token punctuation">&gt;</span>
+            </span>
+            <span class="token tag">
+                <span class="token tag">
+                    <span class="token punctuation">&lt;/</span>span
+                </span>
+                <span class="token punctuation">&gt;</span>
+            </span>`;
+
+        return `           
+            <div class="display-flex align-items-stretch theme-dark border-top">
+
+                <div class="display-flex width-25 d border-right padding-2">
+                    <div class="margin-y-auto text-align-center width-100">
+                        <span class="nyc_icon_${name} display-inline-block"></span>
                     </div>
                 </div>
-            </div>
 
-            <div class=" flex-fill">
-                <pre>
-                    <code class="language-html">
-                        &lt;span class="nyc_icon_${name}"&gt;&lt;/span&gt;
-                    </code>
-                </pre>
-            </div>
-            
-        </div>`;
+                <div class="width-75 padding-x-2 padding-y-3">
+                    <p class="font-size-md margin-bottom-1">Tags: ${tags.map((cool) => cool.replace(',', '')).join(', ')}</p>
+                    <div class="font-size-md">
+                        ${htmlSpan.split('\n').map((s) => s.trim()).join('')}
+                    </div>
+                </div>
+
+            </div>`;
 
     }).join('');
 
