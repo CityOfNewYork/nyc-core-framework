ae4da4caab337a73b43f3e460b03ee900cddbb66
Carl Avidano <cavidano@gmail.com>
2021-01-20 09:19:00 -0500

    adding tooltips

--- src/modules/button/index.js
+++ src/modules/button/index.js
@@ -19,10 +19,45 @@ export default class Button {
                 ${tooltipText}
             </span>`;
 
-            if(tooltipText != null){
-                iconOnlyButton.insertAdjacentHTML('beforeend', tooltipHTML);
-            }
+            if(tooltipText) {
 
+                iconOnlyButton.insertAdjacentHTML('beforeend', tooltipHTML);
+                
+                const buttonTooltip = iconOnlyButton.querySelector(".button__tooltip");
+
+                const buttonTooltipWidth = buttonTooltip.offsetWidth / 2;
+                const buttonPositionLeft = iconOnlyButton.offsetLeft ;
+                const buttonPositionRight = window.innerWidth - (iconOnlyButton.offsetLeft + iconOnlyButton.offsetWidth);
+
+                if (buttonTooltipWidth > buttonPositionLeft) {
+                    buttonTooltip.classList.add("left");
+                }
+
+                if (buttonTooltipWidth > buttonPositionRight) {
+                    buttonTooltip.classList.add("right");
+                }
+
+                // iconOnlyButton.addEventListener("click", (event) => {
+
+                //     event.preventDefault();
+
+                //     console.log(`
+                //         I am ${iconOnlyButton.offsetWidth} pixels wide
+                //         My Tooltip is ${buttonTooltipWidth} pixels wide
+                //         I am ${buttonPositionLeft} from the left
+                //         I am ${buttonPositionRight} from the right
+                //     `);
+
+                //     if (buttonTooltipWidth > buttonPositionLeft) {
+
+                //         console.log("I AM TOO BIG!!");
+
+                //         buttonTooltip.classList.add("cool");
+
+                //     }
+                // });
+            }
+            
         });
     }
 }
