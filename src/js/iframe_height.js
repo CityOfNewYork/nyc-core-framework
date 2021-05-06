3476be389dff867602853e91525ad1f1bb2b9f59
Carl Avidano <cavidano@gmail.com>
2021-05-06 06:26:05 -0400

    fixed iframe height issue!

--- src/js/iframe_height.js
+++ src/js/iframe_height.js
@@ -1,23 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
 
     const iframeList = document.getElementsByTagName("iframe");
 
     for (const iframe of iframeList) {
 
-        const iframeWindow = iframe.contentWindow;
-
         const getIframeDocHeight = () => {
 
-            alert("Whoa")
+            let iframeWindowHeight = iframe.contentWindow.document.body.offsetHeight;
 
-            let iframeWindowHeight = iframeWindow.document.body.scrollHeight;
+            iframe.style.height = iframeWindowHeight + "px";
 
-            iframe.style.height = iframeWindowHeight + 'px';
         }
 
-        iframe.addEventListener('ready', getIframeDocHeight);
-        window.addEventListener('resize', getIframeDocHeight);
-
+        iframe.addEventListener("load", getIframeDocHeight);
+        window.addEventListener("resize", getIframeDocHeight);
     }
-
 });
