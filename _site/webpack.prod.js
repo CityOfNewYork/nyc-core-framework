1bfc8c0a4d7126089b026a83ee69601c80213c44
Carl Avidano <cavidano@gmail.com>
2021-06-17 06:58:36 -0400

    before updating aspect

---
+++ _site/webpack.prod.js
@@ -0,0 +1,70 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+const path = require('path');
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = merge(common, {
+    mode: 'production',
+    target: 'browserslist',
+    output: {
+        filename: 'js/[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            [
+                                "@babel/preset-env",
+                                {
+                                    "useBuiltIns": "usage",
+                                    "corejs": 3
+                                }
+                            ]
+                        ],
+                        plugins: [
+                            "@babel/plugin-transform-shorthand-properties",
+                            "@babel/plugin-proposal-object-rest-spread"
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    [
+                                        "autoprefixer"
+                                    ],
+                                ],
+                            },
+                        },
+                    },
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "css/[name].css",
+        }),
+        new CssMinimizerPlugin()
+    ]
+});
